afa214cb6fd8e796c1a6b1fbb7435c2bf8bd54d0
Bishal <baniyabishal007@>
2026-03-24 00:35:25 +0545

    Added User management methods in Store class

--- store.py
+++ store.py
@@ -50,3 +50,25 @@ class Store:
         self.products[id].stock = stock
 
 
+  # creates new users and adds it to self.users
+  def register_users(self, id, name, password, role):
+    if id not in self.users:
+      my_user = user.User(id, name, password, role)
+      self.users[id] = my_user
+      # self.users.update({id : my_user})
+
+    else:
+      raise ValueError("User already exists!")
+    
+  # finds the user and verifies their password
+  def login_user(self, id, password):
+    if id not in self.users:
+      raise ValueError("User does not exist!")
+    
+    else:
+      if self.users[id].check_password(password):   # checking if the password match 
+        return self.users[id]        # returning user object
+      
+      else:
+        raise ValueError("Incorrect password!")
+      
